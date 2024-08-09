405e6a44bdbc111b8a9307a5b538d4dff2cde8fb
Irasov <srnan@yandex.ru>
2024-08-09 19:59:15 +0500

    feat: add functional brafunction check

--- src/index.js
+++ src/index.js
@@ -1,3 +1,41 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let open = []; 
+  for(let i = 0; i < str.length; i++){
+    for(let j = 0; j < bracketsConfig.length; j++ ){
+      if(str[i] == bracketsConfig[j][0]){
+        if(open.length != 0) {
+          if(open[open.length-1][1] != str[i]){
+            open.push(bracketsConfig[j]);
+          } else if(str[i] == bracketsConfig[j][1]) {
+            if(open.length != 0){
+              if (open[open.length-1][1] == str[i]){
+                open.pop();
+             } else {
+                return false;
+              }
+           } else {
+              return false;
+            }
+          }
+        } else if(open.length == 0) {
+          open.push(bracketsConfig[j]);
+        }
+      } else if(str[i] == bracketsConfig[j][1]) {
+        if(open.length != 0){
+          if (open[open.length-1][1] == str[i]){
+            open.pop();
+         } else {
+            return false;
+          }
+       } else {
+          return false;
+        }
+      }
+    }
+  }
+  if(open.length == 0) {
+    return true;
+  } else {
+    return false;
+  }
 }
